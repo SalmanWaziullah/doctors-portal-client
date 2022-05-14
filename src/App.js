46106bcf45b585080faa37cbaa2f75c379438399
Salman Waziullah <salmanwaziullah1997@gmail.com>
2022-05-15 01:37:58 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Navbar from './pages/Shared/Navbar/Navbar';
+import { Route, Routes } from 'react-router-dom';
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import Appointment from './pages/Appointment/Appointment'
+import Reviews from './pages/Reviews/Reviews';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/appointment" element={<Appointment />} />
+        <Route path="/Reviews" element={<Reviews />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/pages/Appointment/Appointment.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Appoinment = () => {
+    return (
+        <div>
+            <h2>appoinment</h2>
+        </div>
+    );
+};
+
+export default Appoinment;
